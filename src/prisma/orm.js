4c4a5e4ed5e91e557e26bf77f96eaedd89f73d00
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2024-01-01 15:13:34 -0600

    🔧 Add dotenv package and update Prisma ORM
    This commit adds the `dotenv` package to the project dependencies in `package.json`. It also updates the `src/prisma/orm.js` file to include a check for `Prisma.dmmf` before iterating over the models and fields. This ensures that the code runs without errors when using Prisma ORM.

--- src/prisma/orm.js
+++ src/prisma/orm.js
@@ -1,16 +1,20 @@
 import { Prisma } from '@prisma/client';
 const obj = {};
-const prismaModels = Prisma.dmmf.datamodel.models;
-for(const model of prismaModels) {
-	const modelName = model.name;
 
-	if(modelName) {
-		// first letter to lowercase
-		let camelCaseModelName = modelName.charAt(0).toLowerCase() + modelName.slice(1);
-		obj[camelCaseModelName] = {};
-		for(const field of model.fields) {
-			const fieldName = field.name;
-			obj[camelCaseModelName][fieldName] = true;
+if(Prisma.dmmf) {
+
+	const prismaModels = Prisma.dmmf.datamodel.models;
+	for(const model of prismaModels) {
+		const modelName = model.name;
+
+		if(modelName) {
+			// first letter to lowercase
+			let camelCaseModelName = modelName.charAt(0).toLowerCase() + modelName.slice(1);
+			obj[camelCaseModelName] = {};
+			for(const field of model.fields) {
+				const fieldName = field.name;
+				obj[camelCaseModelName][fieldName] = true;
+			}
 		}
 	}
 }
